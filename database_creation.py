709917a578d9d2c65a48a5701916e43c20fbc74b
Alwin <alwinng420@gmail.com>
2026-07-10 12:19:49 -0400

    ??

--- database_creation.py
+++ database_creation.py
@@ -6,17 +6,15 @@ from song_input import mic_input, mp3_input
 
 import os
 
-# database.reset()
-# folder = "song_data"
-# id_count = 0
-# for root, dirs, files in os.walk(folder):
-#     for name in files:
-#         song_path = os.path.join(root,name)
-#         samples, sample_rate = mp3_input(song_path)
-#         log_spec, freqs, times = make_spectrogram(samples,sample_rate)
-#         fgp = make_fgp(find_peaks(log_spec))
-#         database.add(fgp, str(id_count),name[:-4])
-#         print("Added " + name[:-4])
-#         id_count = id_count + 1
-
-database.viewSongs()
+database.reset()
+folder = "song_data"
+id_count = 0
+for root, dirs, files in os.walk(folder):
+    for name in files:
+        song_path = os.path.join(root,name)
+        samples, sample_rate = mp3_input(song_path)
+        log_spec, freqs, times = make_spectrogram(samples,sample_rate)
+        fgp = make_fgp(find_peaks(log_spec))
+        database.add(fgp, str(id_count),name[:-4])
+        print("Added " + name[:-4])
+        id_count = id_count + 1      
